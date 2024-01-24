38b6b40b5ba36f74df5449d0eef6f2b9c07548d6
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-01-24 08:02:04 +0530

    Secondary bar updated
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- components/Home/Secbar.tsx
+++ components/Home/Secbar.tsx
@@ -9,35 +9,34 @@ function Secbar() {
   };
 
   return (
-    <div className='flex flex-row justify-start items-center bg-[#232f3e] py-2 px-4 text-white'>
+    <div className='bg-[#232f3e] px-4 py-2 text-white'>
       <div className='hidden sm:flex'>
-        <div className='text-sm flex flex-row justify-between items-center bg-[#232f3e] py-1 px-2'>
-          <p className='px-2'>All</p>
-          <p className='px-2'>Today's Deals</p>
-          <p className='px-2'>Customer Service</p>
-          <p className='px-2'>Registry</p>
-          <p className='px-2'>Gift Cards</p>
-          <p className='px-2'>Sell</p>
+        <div className='text-sm flex flex-row justify-between items-center bg-[#232f3e]'>
+          <p >All</p>
+          <p >Today's Deals</p>
+          <p >Customer Service</p>
+          <p >Registry</p>
+          <p >Gift Cards</p>
+          <p >Sell</p>
         </div>
       </div>
 
       <div
         onClick={toggleNav}
-        className='sm:hidden  show w-5 h-5 cursor-pointer text-white'
+        className='sm:hidden show w-5 h-5 cursor-pointer text-white'
         role='button'
-      >
-        {/* Consider using a hamburger menu icon here */}☰
+      >☰
       </div>
 
       {isNavOpen && (
         <div className='sm:hidden'>
-          <div className='text-sm md:flex-row justify-between items-center bg-[#232f3e] py-2 px-2'>
-            <p className='px-2 sm:p-2 '>All</p>
-            <p className='px-2 sm:p-2 '>Today's Deals</p>
-            <p className='px-2 sm:p-2 '>Customer Service</p>
-            <p className='px-2 sm:p-2 '>Registry</p>
-            <p className='px-2 sm:p-2 '>Gift Cards</p>
-            <p className='px-2 sm:p-2 '>Sell</p>
+          <div className='text-sm md:flex-row justify-between items-center bg-[#232f3e]'>
+            <p>All</p>
+            <p>Today's Deals</p>
+            <p>Customer Service</p>
+            <p>Registry</p>
+            <p>Gift Cards</p>
+            <p>Sell</p>
           </div>
         </div>
       )}
